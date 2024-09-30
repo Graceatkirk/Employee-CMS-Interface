6b32db61018696b77211d53c07500cce48cb36af
Graceatkirk <grace@kirk.org>
2024-09-30 14:02:22 -0500

    third commit

--- Develop/src/connections.ts
+++ Develop/src/connections.ts
@@ -1,80 +1,22 @@
-import inquirer from 'inquirer';
-import { Client } from 'pg';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-const client = new Client({
+// Load environment variables from .env file
+dotenv.config();
+
+// Create a connection pool using the configuration from .env
+const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  port: Number(process.env.DB_PORT) || 5432, // Default PostgreSQL port
 });
 
-client.connect();
+// Function to query the database
+const query = (text: string, params?: any[]) => {
+  return pool.query(text, params);
+};
 
-export function viewDepartments() {
-  client.query('SELECT * FROM departments', (err, res) => {
-    if (err) throw err;
-    console.table(res.rows);
-    mainMenu();
-  });
-}
-
-export function viewRoles() {
-  client.query(`
-    SELECT roles.id, roles.title, roles.salary, departments.name AS department 
-    FROM roles 
-    JOIN departments ON roles.department_id = departments.id
-  `, (err, res) => {
-    if (err) throw err;
-    console.table(res.rows);
-    mainMenu();
-  });
-}
-
-export function viewEmployees() {
-  client.query(`
-    SELECT e.id, e.first_name, e.last_name, roles.title, departments.name AS department, roles.salary,
-           CONCAT(m.first_name, ' ', m.last_name) AS manager
-    FROM employees e
-    JOIN roles ON e.role_id = roles.id
-    JOIN departments ON roles.department_id = departments.id
-    LEFT JOIN employees m ON e.manager_id = m.id
-  `, (err, res) => {
-    if (err) throw err;
-    console.table(res.rows);
-    mainMenu();
-  });
-}
-
-export function addDepartment() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Enter the department name:',
-    },
-  ]).then((answer) => {
-    client.query('INSERT INTO departments (name) VALUES ($1)', [answer.name], (err) => {
-      if (err) throw err;
-      console.log('Department added!');
-      mainMenu();
-    });
-  });
-}
-
-export function addRole() {
-  // Implementation for adding a role
-}
-
-export function addEmployee() {
-  // Implementation for adding an employee
-}
-
-export function updateEmployeeRole() {
-  // Implementation for updating an employee's role
-}
-
-function mainMenu() {
-    throw new Error('Function not implemented.');
-}
-
+// Export the pool and query function
+export { pool, query };
--- Develop/src/server.ts
+++ Develop/src/server.ts
@@ -1,53 +1,172 @@
+import express from 'express';
 import inquirer from 'inquirer';
-import { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole } from './connections';
+import { query } from './connections'; // Import the query function from connections.ts
 
-function mainMenu() {
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'action',
-        message: 'What would you like to do?',
-        choices: [
-          'View All Departments',
-          'View All Roles',
-          'View All Employees',
-          'Add a Department',
-          'Add a Role',
-          'Add an Employee',
-          'Update an Employee Role',
-          'Exit',
-        ],
-      },
-    ])
-    .then((answer) => {
-      switch (answer.action) {
-        case 'View All Departments':
-          viewDepartments();
-          break;
-        case 'View All Roles':
-          viewRoles();
-          break;
-        case 'View All Employees':
-          viewEmployees();
-          break;
-        case 'Add a Department':
-          addDepartment();
-          break;
-        case 'Add a Role':
-          addRole();
-          break;
-        case 'Add an Employee':
-          addEmployee();
-          break;
-        case 'Update an Employee Role':
-          updateEmployeeRole();
-          break;
-        default:
-          console.log('Goodbye!');
-          process.exit();
-      }
-    });
-}
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-mainMenu();
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Function to start the application
+const startApp = async () => {
+  const options = [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee role',
+    'Exit'
+  ];
+
+  const { option } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'option',
+      message: 'Select an option:',
+      choices: options,
+    },
+  ]);
+
+  switch (option) {
+    case 'View all departments':
+      await viewDepartments();
+      break;
+    case 'View all roles':
+      await viewRoles();
+      break;
+    case 'View all employees':
+      await viewEmployees();
+      break;
+    case 'Add a department':
+      await addDepartment();
+      break;
+    case 'Add a role':
+      await addRole();
+      break;
+    case 'Add an employee':
+      await addEmployee();
+      break;
+    case 'Update an employee role':
+      await updateEmployeeRole();
+      break;
+    case 'Exit':
+      console.log('Exiting application...');
+      process.exit();
+  }
+
+  // Restart the app after handling the selected option
+  startApp();
+};
+
+// Function to view all departments
+const viewDepartments = async () => {
+  const res = await query('SELECT * FROM departments;');
+  console.table(res.rows);
+};
+
+// Function to view all roles
+const viewRoles = async () => {
+  const res = await query('SELECT * FROM roles;');
+  console.table(res.rows);
+};
+
+// Function to view all employees
+const viewEmployees = async () => {
+  const res = await query('SELECT * FROM employees;');
+  console.table(res.rows);
+};
+
+// Function to add a department
+const addDepartment = async () => {
+  const { departmentName } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter the name of the department:',
+    },
+  ]);
+
+  await query('INSERT INTO departments (name) VALUES ($1);', [departmentName]);
+  console.log(`Department ${departmentName} added successfully.`);
+};
+
+// Function to add a role
+const addRole = async () => {
+  const { roleName, salary, departmentId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'roleName',
+      message: 'Enter the name of the role:',
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'Enter the salary for this role:',
+    },
+    {
+      type: 'input',
+      name: 'departmentId',
+      message: 'Enter the department ID for this role:',
+    },
+  ]);
+
+  await query('INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3);', [roleName, salary, departmentId]);
+  console.log(`Role ${roleName} added successfully.`);
+};
+
+// Function to add an employee
+const addEmployee = async () => {
+  const { firstName, lastName, roleId, managerId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter the employee\'s first name:',
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter the employee\'s last name:',
+    },
+    {
+      type: 'input',
+      name: 'roleId',
+      message: 'Enter the role ID for this employee:',
+    },
+    {
+      type: 'input',
+      name: 'managerId',
+      message: 'Enter the manager ID for this employee (leave blank if none):',
+    },
+  ]);
+
+  await query('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4);', [firstName, lastName, roleId, managerId || null]);
+  console.log(`Employee ${firstName} ${lastName} added successfully.`);
+};
+
+// Function to update an employee's role
+const updateEmployeeRole = async () => {
+  const { employeeId, newRoleId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'employeeId',
+      message: 'Enter the ID of the employee to update:',
+    },
+    {
+      type: 'input',
+      name: 'newRoleId',
+      message: 'Enter the new role ID for this employee:',
+    },
+  ]);
+
+  await query('UPDATE employees SET role_id = $1 WHERE id = $2;', [newRoleId, employeeId]);
+  console.log(`Employee ID ${employeeId} updated to role ID ${newRoleId} successfully.`);
+};
+
+// Start the application
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  startApp(); // Start the inquirer prompt
+});
